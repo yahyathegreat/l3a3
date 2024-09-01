37125d2ed93d29b358fbf4a9cdadc2c72d531ab1
yahyathegreat <yahyamardan1234@gmail.com>
2024-09-01 13:20:25 +0500

    l3a3

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# let's check the datatype of diferent values
 a=5
 print("type of a:", type(a))
 b=2.5
@@ -7,3 +6,20 @@ c="coding"
 print("type of c:",type (c))
 d=True
 print("type of d:", type (d))
+name = "penguin"
+age = 15
+is_student = True
+weight = 57.9
+print("name:", name)
+print("datatype of name is:", type(name))
+print("age :", age)
+print("dattype of age is", type (age))
+print("is_student:", is_student)
+print("data type of is_student is", type(is_student))
+print("data type of weight is", type(weight))
+age = str(age)
+print(age)
+print ("data type of age is", type (age))
+weight = int ( weight)
+print(weight)
+print("datatype of weight is", type (weight))
