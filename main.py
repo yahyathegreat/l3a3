d4e2864e74bcf6de829a5375ab51498d32478dcc
yahyathegreat <yahyamardan1234@gmail.com>
2024-09-01 12:54:32 +0500

    l3a3

---
+++ main.py
@@ -0,0 +1,9 @@
+# let's check the datatype of diferent values
+a=5
+print("type of a:", type(a))
+b=2.5
+print("type of b:", type(b))
+c="coding"
+print("type of c:",type (c))
+d=True
+print("type of d:", type (d))
